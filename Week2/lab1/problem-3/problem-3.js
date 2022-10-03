d396bf72aef816efbbbb19d46fd16f0afe513b83
Raphael <raphacabangbang@gmail.com>
2022-10-03 12:37:41 +0100

    created notes still need to work on the edit and delete function

--- Week2/lab1/problem-3/problem-3.js
+++ Week2/lab1/problem-3/problem-3.js
@@ -1,4 +1,35 @@
-const container = document.getElementById("notes-area");
+var id = 0;
+document.getElementById("Add").addEventListener("click", addNote);
 
-const text = document.createTextNode("test");
-container.appendChild(text);
+function addNote() {
+  const text = document.getElementById("title").value;
+  const desc = document.getElementById("description").value;
+  var notediv = document.createElement("div");
+  var notep = document.createElement("p");
+  let editBtn = document.createElement("button");
+  let delBtn = document.createElement("button");
+
+  notediv.id = "note_div" + id;
+  notep.id = "note_p" + id;
+
+  editBtn.id = "editBtn" + id;
+  delBtn.id = "delBtn" + id;
+  id++;
+
+  notediv.innerHTML = text;
+  notep.innerHTML = desc;
+  editBtn.innerHTML = "Edit";
+  delBtn.innerHTML = "Delete";
+
+  document.getElementById("notesArea").appendChild(notediv);
+  notediv.contentEditable = true;
+  document.getElementById("notesArea").appendChild(notep);
+  document.getElementById("notesArea").appendChild(editBtn);
+  document.getElementById("notesArea").appendChild(delBtn);
+}
+document.getElementById("delBtn").addEventListener("click", deleteNote);
+
+function deleteNote() {
+  const parent = document.getElementById("notesArea");
+  parent.removeChild(parent.firstElementChild);
+}
