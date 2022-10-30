f07844a3158ad13131208b06bfa7b47a6e378f0d
Cabangbang <raphacabangbang@gmail.com>
2022-10-30 18:54:14 +0000

    Complete Problem 3

--- Week4/Lab4/Problem-3/Problem-3.js
+++ Week4/Lab4/Problem-3/Problem-3.js
@@ -13,8 +13,41 @@ function getUsers() {
       document.getElementById(
         "profilePicture"
       ).innerHTML = `<img src="${data.avatar_url}" />`;
-      let repos = data.repos_url;
-      document.getElementById("repos").innerHTML = repos.name;
+
+      for (let i = 0; i < data.public_repos; i++) {
+        //!Deletes the repos
+        document.getElementById("userRepos").innerHTML = " ";
+      }
+
+      getUsersRepos(data, name); //*Retrieves the repos to display
+    })
+  );
+}
+
+function getUsersRepos(user, name) {
+  fetch(`https://api.github.com/users/${name}/repos`).then((response) =>
+    response.json().then((data) => {
+      console.log(data);
+      for (let i = 0; i < user.public_repos; i++) {
+        test = data[i];
+
+        let repoName = document.createElement("h4");
+        let repoDesc = document.createElement("h4");
+        let repoDiv = document.createElement("div");
+
+        repoDiv.id = "repoDiv " + i;
+        repoName.innerHTML = "Name: " + data[i].name;
+        repoDesc.innerHTML = "Description: " + data[i].description;
+
+        document.getElementById("userRepos").appendChild(repoDiv); //*Creates a div and styles it
+        repoDiv.setAttribute("style", "border: #CCCCCC 1px solid");
+        document
+          .getElementById("userRepos")
+          .setAttribute("style", "overflow-x: auto");
+
+        document.getElementById("repoDiv " + i).appendChild(repoName); //*Appends the name and descriptions to the created div
+        document.getElementById("repoDiv " + i).appendChild(repoDesc);
+      }
     })
   );
 }
