42ccad32ec1a9bf0ec081c3da8da97f321be9011
Raphael <raphacabangbang@gmail.com>
2022-11-29 20:58:40 +0000

    fixing the css of problem 2

--- Week8/Problem 2/problem-2.js
+++ Week8/Problem 2/problem-2.js
@@ -9,10 +9,11 @@ const startTimer = {
     const min = document.getElementById("minute").value;
     const sec = document.getElementById("second").value;
 
-    const hrs = hour * 3600;
-    const mins = min * 60;
+    const timeHrs = parseInt(hour * 60 * 60);
+    const timeMin = parseInt(min * 60);
+    const timeSec = parseInt(sec);
 
-    const totaltime = hrs + mins + sec;
+    const totaltime = timeHrs + timeMin + timeSec;
 
     startCount = Rx.Observable.interval(1000)
       .map((count) => totaltime - count)
@@ -20,8 +21,8 @@ const startTimer = {
         var time = String(seconds / 3600);
         var now = new Date(0, 0);
         now.setSeconds(+time * 60 * 60);
-
         timerDisplay.innerHTML = now.toTimeString().slice(0, 8);
+        timerDisplay.style.fontSize = "20px";
         if (time == 0) {
           timerDisplay.innerHTML = "Time is up";
           startCount.unsubscribe();
